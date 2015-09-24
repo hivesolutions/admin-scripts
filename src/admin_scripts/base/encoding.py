f528aad7257211e22432c26408743533f9766607
joamag <joamag@gmail.com>
2015-09-24 22:31:19 +0100

    new usage message

--- src/admin_scripts/base/encoding.py
+++ src/admin_scripts/base/encoding.py
@@ -45,9 +45,14 @@ import legacy
 
 import admin_scripts.extra as extra
 
-USAGE_MESSAGE = "encoding path [-r] [-s source_encoding] [-t target_encoding] [-u] [-x replacement_from_1,\
-replacement_to_1, replacement_from_2, replacement_to_2, ...] [-e file_extension_1, file_extension_2, ...]\
-[-w exclusion_1, exclusion_2, ...] [-c configuration_file]"
+USAGE_MESSAGE = "encoding path [-r]\
+[-s source_encoding]\
+[-t target_encoding]\
+[-u]\
+[-x replacement_from_1, replacement_to_1, replacement_from_2, replacement_to_2, ...]\
+[-e file_extension_1, file_extension_2, ...]\
+[-w exclusion_1, exclusion_2, ...]\
+[-c configuration_file]"
 """ The usage message, to be printed when help is required
 or when a command line error exists """
 
