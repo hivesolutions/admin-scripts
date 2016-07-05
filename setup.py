1b7333330dc4461f98a2500022e86c7e63f23004
joamag <joamag@hive.pt>
2016-07-05 12:55:57 +0100

    new version value

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "admin_scripts",
-    version = "0.3.19",
+    version = "0.3.20",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Administration Scripts",
