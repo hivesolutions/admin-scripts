80aa14cc0d8fd9edd3a27583880b80631a189282
João Magalhães <joamag@hive.pt>
2018-10-24 14:51:08 +0100

    new version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "admin_scripts",
-    version = "0.5.8",
+    version = "0.5.9",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Administration Scripts",
