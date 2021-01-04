1ece26636f05216d9f6e1b09216ba873180f6aa9
João Magalhães <joamag@gmail.com>
2021-01-04 10:42:51 +0000

    Version 0.6.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "admin_scripts",
-    version = "0.5.16",
+    version = "0.6.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Administration Scripts",
